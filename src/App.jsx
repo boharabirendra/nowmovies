a4b98953c81f2a70a036222def419200443e9cbe
boharabirendra <boharabirendra546@gmail.com>
2025-02-17 23:02:29 +0545

    Make MovieCard responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
+import { Box, Grid, Typography } from "@mui/material";
 import MovieCard from "./components/MovieCard";
 
 function App() {
   return (
-    <>
-      <MovieCard />
-    </>
+    <Grid
+      container
+      spacing={8}
+      sx={{
+        padding: 3,
+      }}
+    >
+      <Grid item xs={12} lg={6}>
+        <Typography
+          variant="h4"
+          textAlign={"center"}
+          sx={{ fontSize: "2.125rem", marginBottom: "32px" }}
+        >
+          Trending Movies
+        </Typography>
+        <Box display={"flex"} flexDirection={"column"} gap={2}>
+          <MovieCard />
+        </Box>
+      </Grid>
+      <Grid item xs={12} lg={6}>
+        <Typography
+          variant="h4"
+          textAlign={"center"}
+          sx={{ fontSize: "2.125rem", marginBottom: "32px" }}
+        >
+          Trending TV Shows
+        </Typography>
+
+        <Box display={"flex"} flexDirection={"column"} gap={2}>
+          <MovieCard />
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,9 +7,9 @@ import {
   Typography,
   ListItemText,
 } from "@mui/material";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import SearchIcon from "@mui/icons-material/Search";
 import Stack from "@mui/material/Stack";
+import SearchIcon from "@mui/icons-material/Search";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
 export default function MovieCard() {
   return (
@@ -18,10 +18,11 @@ export default function MovieCard() {
         color: "white",
         borderRadius: "10px",
         position: "relative",
+        width: "100%",
         cursor: "pointer",
         transition: "transform 0.3s ease-in-out",
         "&:hover": {
-          transform: "scale(1.01)",
+          transform: "scale(1.03)",
         },
       }}
     >
@@ -32,9 +33,20 @@ export default function MovieCard() {
           padding: "16px",
           background:
             "linear-gradient(to right, #0d0d0c 50%, transparent 100%)",
+          "@media (max-width: 968px)": {
+            background:
+              "linear-gradient(to top, #0d0d0c 50%, transparent 100%)",
+          },
         }}
       >
-        <Box width={"60%"}>
+        <Box
+          width={"60%"}
+          sx={{
+            "@media (max-width: 968px)": {
+              width: "100%",
+            },
+          }}
+        >
           <Typography
             variant="h4"
             sx={{ textTransform: "capitalize", "&:hover": { color: "red" } }}
@@ -59,6 +71,7 @@ export default function MovieCard() {
         <Box
           display={"flex"}
           alignItems={"center"}
+          flexWrap={"wrap"}
           gap={0.5}
           sx={{ marginTop: "16px" }}
         >
@@ -73,7 +86,14 @@ export default function MovieCard() {
             2024-02-10
           </Typography>
           <List
-            sx={{ display: "flex", alignItems: "center", marginLeft: "16px" }}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              marginLeft: "16px",
+              "@media (max-width: 968px)": {
+                marginLeft: "0px",
+              },
+            }}
           >
             {[
               { genre: "Action" },
@@ -115,7 +135,15 @@ export default function MovieCard() {
             ))}
           </List>
         </Box>
-        <Box marginTop={8} width={"50%"}>
+        <Box
+          marginTop={8}
+          width={"50%"}
+          sx={{
+            "@media (max-width: 968px)": {
+              width: "100%",
+            },
+          }}
+        >
           <Typography component={"p"}>
             Two highly trained operatives grow close from a distance after being
             sent to guard opposite sides of a mysterious gorge. When an evil
@@ -123,7 +151,16 @@ export default function MovieCard() {
           </Typography>
         </Box>
         <Box marginTop={4}>
-          <Stack direction="row" spacing={2}>
+          <Stack
+            direction="row"
+            gap={2}
+            sx={{
+              "@media (max-width: 968px)": {
+                flexDirection: "column",
+                gap: "16px",
+              },
+            }}
+          >
             <Box
               component={"a"}
               variant="contained"
@@ -134,6 +171,7 @@ export default function MovieCard() {
                 gap: "8px",
                 transition: "all 0.3s",
                 backgroundColor: "#FF0000",
+                justifyContent: "center",
                 fontWeight: "500",
                 padding: "5px 15px",
                 borderRadius: "4px",
@@ -147,7 +185,10 @@ export default function MovieCard() {
               title={
                 <Typography
                   component={"p"}
-                  sx={{ fontSize: "14px", fontWeight: "500" }}
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: "500",
+                  }}
                 >
                   Heuristic. Might show empty results.
                 </Typography>
@@ -161,6 +202,7 @@ export default function MovieCard() {
                 sx={{
                   display: "flex",
                   fontSize: "0.875rem",
+                  justifyContent: "center",
                   alignItems: "center",
                   gap: "8px",
                   transition: "all 0.3s",
@@ -169,6 +211,7 @@ export default function MovieCard() {
                   padding: "5px 15px",
                   borderRadius: "4px",
                   border: "1px solid #6ac045",
+                  marginLeft: 0,
                   "&:hover": {
                     border: "1px solid #3f57b5",
                     backgroundColor: "rgba(63, 81, 181, 0.04)",
@@ -194,6 +237,9 @@ export default function MovieCard() {
           position: "absolute",
           borderRadius: "11px",
           backgroundSize: "cover !important",
+          "@media (max-width: 968px)": {
+            width: "100%",
+          },
         }}
       ></Box>
     </Card>
